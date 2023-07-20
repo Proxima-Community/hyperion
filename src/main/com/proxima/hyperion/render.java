72d3b9515b518090e2e3b306c808a79ba1015153
Finn2763 <finleyjaerling@gmail.com>
2023-07-20 16:36:54 -0400

    added render method to project points

---
+++ src/main/com/proxima/hyperion/render.java
@@ -0,0 +1,20 @@
+package src.main.com.proxima.hyperion;
+
+public class render {
+
+    public Vector2 render(Vector4 vctr) {
+
+        float[] hi = vctr.values;
+
+        float x = vctr.values[1];
+        float y = vctr.values[2];
+        float z = vctr.values[3];
+
+        float xproj = x/z;
+        float yproj = y/z;
+        
+        return new Vector2(xproj, yproj);
+
+    }
+
+}
