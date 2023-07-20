d67df59d1f750afcecdca35225f6d0386cf79ef9
Cheetah <DaverDiscord@gmail.com>
2023-07-20 13:04:49 -0700

    add a rectangle

--- src/main/com/proxima/hyperion/Display.java
+++ src/main/com/proxima/hyperion/Display.java
@@ -5,7 +5,8 @@ import java.awt.*;
 public class Display extends Canvas {
 
     public void paint(Graphics g) {
-        
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, 100, 100);
     }
     
 }
